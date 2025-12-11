8b2f5ec0852f5c23e672909c63dca368f4a54b61
Anatoliy <vladimirovtolik149@gmail.com>
2025-12-11 21:30:09 +0300

    FirstCommit

---
+++ main.py
@@ -0,0 +1,28 @@
+from fastapi import FastAPI
+from datetime import date
+from pydantic import BaseModel
+import uvicorn
+from fastapi import FastAPI
+from pydantic import BaseModel
+
+app = FastAPI()
+
+class User(BaseModel):
+    username: str
+    message: str
+    
+@app.post("/")
+async def root(user: User):
+    """
+    Здесь мы можем с переменной user, которая содержит объект класса User с соответствующими полями,
+    выполнить любую логику – например, сохранить информацию в базу данных, передать в другую функцию и т.д.
+    """
+    print(f'Мы получили от юзера {user.username} такое сообщение: {user.message}')
+    return user
+
+
+
+
+
+
+
